f9c30f9a0bcddc371412cf1a4e3a3c98e25eb390
Frédéric Wang <fred.wang@free.fr>
2014-05-30 20:45:05 +0200

    TeXZilla: use an action button instead of a widget.

--- texzilla/lib/main.js
+++ texzilla/lib/main.js
@@ -3,14 +3,15 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 var data    = require("sdk/self").data;
-var widgets = require("sdk/widget");
+var action  = require("sdk/ui/button/action")
 var tabs    = require("sdk/tabs");
 
-// add a widget to open the TeXZilla editor
-var widget = widgets.Widget({
+action.ActionButton({
   id: "texzilla-editor",
   label: "TeXZilla Editor",
-  contentURL: data.url("logo.png"),
+  icon: {
+    "32": data.url("logo.png"),
+  },
   onClick: function() {
     tabs.open(data.url("index.html"));
   }
